cc640c88e944bff39ac6ff67f1ac8cb4a2149f62
eXenon <xavier.nunn@protonmail.com>
2018-05-15 11:15:49 +0200

    New polybar colors and better gitignore

--- polybar/ping.sh
+++ polybar/ping.sh
@@ -4,6 +4,11 @@ HOST=1.1.1.1
 
 rtt=$(ping $HOST -c 1 | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
 
+if [ -z "$rtt" ]; then
+    echo "%{F#d60606} ø%{F-}"
+    exit 0
+fi
+
 if [ "$rtt" -lt 50 ]; then
     icon="%{F#3cb703}%{F-}"
 elif [ "$rtt" -lt 150 ]; then
