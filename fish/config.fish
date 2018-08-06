3521b493c0f0d8ee41c720bb378ce50bfb12e383
eXenon <xavier.nunn@protonmail.com>
2018-08-06 06:53:57 +0200

    Updated a lot of things

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@
 #   Simple aliases
 #
 function tmux
-	/usr/bin/tmux -2
+	/usr/bin/tmux -2 $argv
 end
 
 function vim
@@ -11,3 +11,5 @@ end
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/cba/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/home/cba/google-cloud-sdk/path.fish.inc'; else; . '/home/cba/google-cloud-sdk/path.fish.inc'; end; end
+
+set -xg PATH ~/.local/bin/ $PATH
