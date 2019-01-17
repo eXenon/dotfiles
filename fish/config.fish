6b805dd9996a3ca684f22fdcf0d51a75952a85de
eXenon <xavier.nunn@protonmail.com>
2019-01-17 12:27:13 +0100

    Removed Google SDK. Should be last commit on this file.

--- fish/config.fish
+++ fish/config.fish
@@ -6,8 +6,4 @@ alias vim="nvim"
 alias txp="tmuxp"
 alias txl="tmuxp load"
 
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/cba/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/home/cba/google-cloud-sdk/path.fish.inc'; else; . '/home/cba/google-cloud-sdk/path.fish.inc'; end; end
-
 set -xg PATH ~/.local/bin/ $PATH
