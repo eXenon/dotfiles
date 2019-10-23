bea4c486fe5c03488dfe7dfc581eb3143876b6fd
eXenon <xavier.nunn@protonmail.com>
2019-10-23 12:37:20 +0200

    Moved laptop functions to host specific file

--- fish/conf.host/xavier.fish
+++ fish/conf.host/xavier.fish
@@ -1,1 +1,19 @@
 set -xg KBFS /home/xavier/kbfs/
+
+# Start X at login
+if test (id -u) -eq 0
+    # Root user. Dont start X. 
+else
+    if status is-login
+        if test -z "$DISPLAY" -a $XDG_VTNR = 1
+            exec ssh-agent startx -- -keeptty
+        end
+    end
+end
+
+# Direnv
+direnv hook fish | source
+
+# Kitty setup
+kitty + complete setup fish | source
+
