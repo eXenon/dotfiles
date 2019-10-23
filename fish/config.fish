af386eca52e31cfa018a0287c2c7a8e2c423d0fb
eXenon <xavier.nunn@protonmail.com>
2019-10-23 12:25:27 +0200

    Added host specific configuration file

--- fish/config.fish
+++ fish/config.fish
@@ -10,7 +10,10 @@ set -xg ERL_AFLAGS "-kernel shell_history enabled"
 set -xg PATH ~/.local/bin/ $PATH
 set -xg EDITOR "/usr/bin/nvim"
 
-set -xg KBFS /home/xavier/kbfs/
+# Load host-specific file
+set -xg HOSTNAME (hostname)
+set -xg FISH_HOST_CONFIG_FILE "$__fish_config_dir/conf.host/$HOSTNAME.fish"
+source $FISH_HOST_CONFIG_FILE
 
 # Start X at login
 if test (id -u) -eq 0
@@ -28,3 +31,8 @@ kitty + complete setup fish | source
 
 # Direnv
 direnv hook fish | source
+
+# Opam
+if test -n "$OPAM_SWITCH_PREFIX"
+    eval (opam env)
+end
