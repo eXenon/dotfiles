6c0db40dc4813e5005d6c65d26b9c6fd093df66f
eXenon <xavier.nunn@protonmail.com>
2019-10-23 12:28:55 +0200

    Moved server functions to host specific file

--- fish/conf.host/ns341786.fish
+++ fish/conf.host/ns341786.fish
@@ -1,1 +1,30 @@
-set -xg KBFS /keybase/
+set -gx PATH ~/.gem/ruby/2.3.0/bin $PATH
+
+set -gx VISUAL nvim
+set -gx EDITOR nvim
+set -gx DBUS_SESSION_BUS_ADDRESS unix:path=/run/user/1000/bus
+
+set -gx LC_ALL en_US.UTF-8
+set -gx LC_CTYPE en_US.UTF-8
+
+set -gx KBFS /keybase/
+
+function tx
+  tmux attach -t txmain; or tmux new -s txmain
+end
+
+function tx-takeover
+  tmux detach -a
+end
+
+function loadavg
+  uptime | grep -o '[0-9]\+\.[0-9]\+' | head -n1
+end
+
+function diskusg
+  df / | tail -n1 | sed "s/  */ /g" | cut -d' ' -f 5 | cut -d'%' -f1
+end
+
+function reload
+  source ~/.config/fish/config.fish
+end
