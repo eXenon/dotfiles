15fc4083c057f8f1ecb9e2dfcf5171476d73cdff
eXenon <xavier.nunn@protonmail.com>
2019-10-23 12:37:20 +0200

    Moved laptop functions to host specific file

--- fish/config.fish
+++ fish/config.fish
@@ -15,23 +15,6 @@ set -xg HOSTNAME (hostname)
 set -xg FISH_HOST_CONFIG_FILE "$__fish_config_dir/conf.host/$HOSTNAME.fish"
 source $FISH_HOST_CONFIG_FILE
 
-# Start X at login
-if test (id -u) -eq 0
-    # Root user. Dont start X. 
-else
-    if status is-login
-        if test -z "$DISPLAY" -a $XDG_VTNR = 1
-            exec ssh-agent startx -- -keeptty
-        end
-    end
-end
-
-# Kitty setup
-kitty + complete setup fish | source
-
-# Direnv
-direnv hook fish | source
-
 # Opam
 if test -n "$OPAM_SWITCH_PREFIX"
     eval (opam env)
