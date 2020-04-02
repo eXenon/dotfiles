1e6236fc5be81893fdc110ceb2ca83ae20d3d948
eXenon <xavier.nunn@protonmail.com>
2020-04-02 08:54:50 +0200

    Moving functions to new host config file

--- fish/conf.host/manatee.fish
+++ fish/conf.host/manatee.fish
@@ -1,1 +1,23 @@
 set -gx KBFS /home/exenon/keybase/
+set -gx CHALET 5.196.1.178
+set -gx TERM xterm
+
+function tx
+  tmux attach -t txmain; or tmux new -s txmain
+end
+
+function tx-takeover
+  tmux detach -a
+end
+
+function loadavg
+  uptime | grep -o '[0-9]\+\.[0-9]\+' | head -n1
+end
+
+function diskusg
+  df / | tail -n1 | sed "s/  */ /g" | cut -d' ' -f 5 | cut -d'%' -f1
+end
+
+function reload
+  source ~/.config/fish/config.fish
+end
