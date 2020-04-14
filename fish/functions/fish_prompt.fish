4d759ffea2341c8365d71cd95059f2d1f38e84a6
eXenon <xavier.nunn@gmail.com>
2020-04-14 08:02:22 +0200

    Better shell

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -53,61 +53,83 @@ set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 
 function __user_color --desc "Return background color depending on active user"
-    switch $USER
-    case root
-        echo AA5555
-    case exenon xavier
-        echo $color2
-    case '*'
-        echo $color4
-    end
+  switch $USER
+  case root
+    echo FF0000
+  case exenon xavier
+    echo $color2
+  case '*'
+    echo $color4
+  end
+end
+
+function __user_prompt --desc "Return small prompt based on user"
+  switch $USER
+  case root
+    echo "🗲"
+  case exenon xavier
+    echo ">"
+  case '*'
+    echo "?"
+  end
 end
 
 function __bubble --desc "Create a powerline segment with color transition"
-    set bg $argv[1]
-    set fg $argv[2]
-    set content $argv[3]
+  set bg $argv[1]
+  set fg $argv[2]
+  set content $argv[3]
 
-    # A starting bubble
-    set_color -b "$background"
-    set_color "$bg"
-    echo -n \uE0B6
+  # A starting bubble
+  set_color -b "$background"
+  set_color "$bg"
+  echo -n \uE0B6
 
-    # Then the content
-    set_color -b "$bg"
-    set_color "$fg"
-    echo -n $content
+  # Then the content
+  set_color -b "$bg"
+  set_color "$fg"
+  echo -n $content
 
-    # And finish the bubble 
-    set_color -b "$background"
-    set_color "$bg"
-    echo -n \uE0B4
+  # And finish the bubble 
+  set_color -b "$background"
+  set_color "$bg"
+  echo -n \uE0B4
 
-    # Reset colors
-    set_color "$foreground"
+  # Reset colors
+  set_color "$foreground"
 end
 
 function fish_prompt
-	echo -n " "
-        __bubble $color4 (__user_color) ">"
-	echo -n " "
+  set -g __prompt_status (echo $status)
+  echo "" # Leave a line
+  echo -n " "
+  __bubble $color0 (__user_color) (__user_prompt)
+  echo -n " "
 end
 
 function fish_right_prompt
-	# Pwd
-	__bubble $color1 $color4 (prompt_pwd)
+  # Git
+  set git_prompt (__fish_git_prompt)
+  if git rev-parse --git-dir > /dev/null 2>&1;
+    __bubble $color8 $color3 $git_prompt
+    echo -n " "
+  end
 
-	echo -n " "
+  # Pwd
+  __bubble $color0 $color4 (prompt_pwd)
+  echo -n " "
 
-	# Root
-	if test $USER = "root"
-		__bubble $color9 $color11 "🗲"
-		echo -n " "
-	end
+  # Background jobs
+  if test (jobs -p | wc -l) -gt 0
+    __bubble $color3 $color0 (jobs -p | wc -l)
+    echo -n " "
+  end
 
-        # Git
-	set git_prompt (__fish_git_prompt)
-	if git rev-parse --git-dir > /dev/null 2>&1;
-		__bubble $color5 $color15 $git_prompt
-	end
+  # Failed commands
+  if test $__prompt_status -eq 127
+    __bubble $color1 $color0 "?"
+    echo -n " "
+  else if test $__prompt_status -ne 0
+    __bubble $color1 $color0 "$__prompt_status"
+    echo -n " "
+  end
 end
