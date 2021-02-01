7547de582c67b6b785dec830bf635a8592128efa
eXenon <xavier.nunn@gmail.com>
2021-02-01 14:26:25 +0100

    Moved screen management to fish script
    The screen choice is now more dynamic to account for changing screen names.

--- fish/conf.host/xavier.fish
+++ fish/conf.host/xavier.fish
@@ -20,3 +20,47 @@ kitty + complete setup fish | source
 
 # Add execs to path
 set -xg PATH /home/xavier/execs:$PATH
+
+# Screen switch functions
+function only_hdmi
+  set external (xrandr | grep " connected" | grep -v eDP | head -n 1 | cut -d" " -f 1)
+  set internal (xrandr | grep " connected" | grep eDP | head -n 1 | cut -d" " -f 1)
+
+  # First step : deactivate the internal screen, then change the scaling.
+  # Turns out i3 has weird behavior when changing everything at once
+  xrandr --output $external --mode 1920x1080 --pos 0x0 --rotate normal --output $internal --off
+
+  # Refresh i3
+  i3-msg restart
+
+  # Set monitor to exernal
+  xrandr --output $external --mode 1920x1080 --pos 0x0 --rotate normal --scale 1.6x1.6 --output $internal --off
+
+  # Set keyboard to bépo
+  setxkbmap fr bepo
+  touch "/tmp/bepo"
+end
+
+function only_internal
+  set external (xrandr | grep " connected" | grep -v eDP | head -n 1 | cut -d" " -f 1)
+  set internal (xrandr | grep " connected" | grep eDP | head -n 1 | cut -d" " -f 1)
+
+  # Set monitor to internal
+  xrandr --output $internal --mode 2560x1440 --pos 0x0 --rotate normal --primary --output $external --off
+
+  # Set keyboard to french
+  setxkbmap fr
+  rm -f "/tmp/bepo"
+end
+
+function both_screens
+  set external (xrandr | grep " connected" | grep -v eDP | head -n 1 | cut -d" " -f 1)
+  set internal (xrandr | grep " connected" | grep eDP | head -n 1 | cut -d" " -f 1)
+
+  # Set monitor to internal
+  xrandr --output $internal --mode 2560x1440 --pos 0x2160 --rotate normal --primary --output $external --mode 1920x1080 --pos 0x0 --rotate normal --scale 1.6x1.6
+
+  # Set keyboard to french
+  setxkbmap fr
+  rm -f "/tmp/bepo"
+end
