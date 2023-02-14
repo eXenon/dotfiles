fb883eaaf0177aff5f531189046ec2b206cd5a13
eXenon <xavier.nunn@protonmail.com>
2023-02-14 06:45:04 +0100

    Add more explicit packages to aoid bootstrapping errors

--- vim/me/packer.lua
+++ vim/me/packer.lua
@@ -1,54 +1,55 @@
 return require("packer").startup(function(use)
-	-- Packer can manage itself!
-	use "wbthomason/packer.nvim"
+    -- Packer can manage itself!
+    use "wbthomason/packer.nvim"
 
-	-- Telescope
-	use {
-		'nvim-telescope/telescope.nvim', branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    -- Telescope
+    use {
+        'nvim-telescope/telescope.nvim', branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
     use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
-	-- Catppuccin Colorscheme
-	use { "catppuccin/nvim", as = "catppuccin" }
+    -- Catppuccin Colorscheme
+    use { "catppuccin/nvim", as = "catppuccin" }
 
-	-- Treesitter & related
-	use { "nvim-treesitter/nvim-treesitter", run = { ':TSUpdate' }}
+    -- Treesitter & related
+    use { "nvim-treesitter/nvim-treesitter", run = { ':TSUpdate' }}
     use "nvim-treesitter/nvim-treesitter-textobjects"
-	use "nvim-treesitter/playground"
+    use "nvim-treesitter/playground"
 
-	-- Undotree
-	use "mbbill/undotree"
+    -- Undotree
+    use "mbbill/undotree"
 
-	-- Git integration
-	use "tpope/vim-fugitive"
+    -- Git integration
+    use "tpope/vim-fugitive"
 
     -- Load neovim lua API
     use "folke/neodev.nvim"
     require("neodev").setup({ }) -- This needs to be set up before the LSP
 
-	-- LSP
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+    -- LSP
+    use "neovim/nvim-lspconfig"
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-buffer'},
-			{'hrsh7th/cmp-path'},
-			{'saadparwaiz1/cmp_luasnip'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'hrsh7th/cmp-nvim-lua'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
 
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},
-			{'rafamadriz/friendly-snippets'},
-		}
-	}
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
+    }
 
     -- Jinja higlighting
     use "hiphish/jinja.vim"
