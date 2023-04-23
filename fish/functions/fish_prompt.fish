a0495c1ca702dc3656fd6e2f2a651a8f9e4d4836
eXenon <xavier.nunn@protonmail.com>
2023-04-23 14:11:51 +0200

    Move to tide prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,156 +1,90 @@
-#
-# Colors
-#
-
-# special
-set foreground F8F8F2
-set background 161D38
-
-# black
-set color0 29315A
-set color8 613E9F
-
-# red
-set color1 E356A7
-set color9 FF79C6
-
-# green
-set color2 42E66C
-set color10 50FA7B
-
-# yellow
-set color3 E4F34A
-set color11 F1FA8C
-
-# blue
-set color4 9B6BDF
-set color12 BD93F9
-
-# magenta
-set color5 E64747
-set color13 FF5555
-
-# cyan
-set color6 44D1EF
-set color14 70D2E6
-
-# white
-set color7 EFA554
-set color15 FFB86C
-
-# Status Chars
-set __fish_git_prompt_char_dirtystate '•'
-set __fish_git_prompt_char_stagedstate '+'
-set __fish_git_prompt_char_stashstate '$'
-set __fish_git_prompt_char_upstream_ahead '▲'
-set __fish_git_prompt_char_upstream_behind '▼'
-set __fish_git_prompt_char_upstream_equal '▸'
-set __fish_git_prompt_char_untrackedfiles '…'
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showstagedstate 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
-
-function __user_color --desc "Return background color depending on active user"
-  switch $USER
-  case root
-    echo FF0000
-  case exenon xavier
-    echo $color2
-  case '*'
-    echo $color4
-  end
-end
-
-function __user_prompt --desc "Return small prompt based on user"
-  switch $USER
-  case root
-    echo "🗲"
-    __vi_mode
-  case exenon xavier xavier.nunn
-    __vi_mode
-  case '*'
-    echo "unknown user"
-  end
-end
-
-function __bubble --desc "Create a powerline segment with color transition"
-  set bg $argv[1]
-  set fg $argv[2]
-  set content $argv[3]
-
-  # A starting bubble
-  set_color -b "$background"
-  set_color "$bg"
-  echo -n \uE0B6
-
-  # Then the content
-  set_color -b "$bg"
-  set_color "$fg"
-  echo -n $content
-
-  # And finish the bubble 
-  set_color -b "$background"
-  set_color "$bg"
-  echo -n \uE0B4
-
-  # Reset colors
-  set_color "$foreground"
-end
-
-function __vi_mode --desc "Translate the vi mode into a single caracter"
-  switch $fish_bind_mode
-    case default
-      echo '_'
-    case insert
-      set_color --bold green
-      echo '>'
-    case replace_one
-      set_color --bold green
-      echo 'r'
-    case visual
-      set_color --bold brmagenta
-      echo 'v'
-    case '*'
-      set_color --bold red
-      echo "unknown mode"
-  end
-  set_color normal
-end
-
 function fish_prompt
-  set -g __prompt_status (echo $status)
-  echo "" # Leave a line
-  echo -n " "
-  __bubble $color0 (__user_color) (__user_prompt)
-  echo -n " "
+end # In case this file gets loaded non-interactively, e.g by conda
+status is-interactive || exit
+
+_tide_remove_unusable_items
+_tide_cache_variables
+_tide_parent_dirs
+source (functions --details _tide_pwd)
+
+set -l prompt_var _tide_prompt_$fish_pid
+set -U $prompt_var # Set var here so if we erase $prompt_var, bg job won't set a uvar
+
+set_color normal | read -l color_normal
+status fish-path | read -l fish_path
+
+# _tide_repaint prevents us from creating a second background job
+function _tide_refresh_prompt --on-variable $prompt_var --on-variable COLUMNS
+    set -g _tide_repaint
+    commandline -f repaint
+end
+
+if contains newline $_tide_left_items # two line prompt initialization
+    test "$tide_prompt_add_newline_before" = true && set -l add_newline '\n'
+
+    set_color $tide_prompt_color_frame_and_connection -b normal | read -l prompt_and_frame_color
+
+    set -l column_offset 5
+    test "$tide_left_prompt_frame_enabled" = true &&
+        set -l top_left_frame "$prompt_and_frame_color╭─" &&
+        set -l bot_left_frame "$prompt_and_frame_color╰─" &&
+        set column_offset (math $column_offset-2)
+    test "$tide_right_prompt_frame_enabled" = true &&
+        set -l top_right_frame "$prompt_and_frame_color─╮" &&
+        set -l bot_right_frame "$prompt_and_frame_color─╯" &&
+        set column_offset (math $column_offset-2)
+
+    eval "
+function fish_prompt
+    _tide_status=\$status _tide_pipestatus=\$pipestatus if not set -e _tide_repaint
+        jobs -q && set -lx _tide_jobs
+        $fish_path -c \"set _tide_pipestatus \$_tide_pipestatus
+set _tide_parent_dirs \$_tide_parent_dirs
+PATH=\$(string escape \"\$PATH\") CMD_DURATION=\$CMD_DURATION fish_bind_mode=\$fish_bind_mode set $prompt_var (_tide_2_line_prompt)\" &
+        builtin disown
+
+        command kill \$_tide_last_pid 2>/dev/null
+        set -g _tide_last_pid \$last_pid
+    end
+
+    math \$COLUMNS-(string length -V \"\$$prompt_var[1][1]\$$prompt_var[1][3]\")+$column_offset | read -lx dist_btwn_sides
+
+    echo -ns $add_newline'$top_left_frame'(string replace @PWD@ (_tide_pwd) \"\$$prompt_var[1][1]\")'$prompt_and_frame_color'
+    string repeat -Nm(math max 0, \$dist_btwn_sides-\$_tide_pwd_len) '$tide_prompt_icon_connection'
+    echo -ns \"\$$prompt_var[1][3]$top_right_frame\"\n\"$bot_left_frame\$$prompt_var[1][2]$color_normal \"
 end
 
 function fish_right_prompt
-  # Git
-  set git_prompt (__fish_git_prompt)
-  if git rev-parse --git-dir > /dev/null 2>&1;
-    __bubble $color8 $color3 $git_prompt
-    echo -n " "
-  end
+    string unescape \"\$$prompt_var[1][4]$bot_right_frame$color_normal\"
+end"
+else # one line prompt initialization
+    test "$tide_prompt_add_newline_before" = true && set -l add_newline '\0'
 
-  # Pwd
-  __bubble $color0 $color4 (prompt_pwd)
-  echo -n " "
+    math 5 -$tide_prompt_min_cols | read -l column_offset
+    test $column_offset -ge 0 && set column_offset "+$column_offset"
 
-  # Background jobs
-  if test (jobs -p | wc -l) -gt 0
-    __bubble $color3 $color0 (jobs -p | wc -l | string trim)
-    echo -n " "
-  end
+    eval "
+function fish_prompt
+    _tide_status=\$status _tide_pipestatus=\$pipestatus if not set -e _tide_repaint
+        jobs -q && set -lx _tide_jobs
+        $fish_path -c \"set _tide_pipestatus \$_tide_pipestatus
+set _tide_parent_dirs \$_tide_parent_dirs
+PATH=\$(string escape \"\$PATH\") CMD_DURATION=\$CMD_DURATION fish_bind_mode=\$fish_bind_mode set $prompt_var (_tide_1_line_prompt)\" &
+        builtin disown
 
-  # Failed commands
-  if test $__prompt_status -eq 127
-    __bubble $color1 $color0 "?"
-    echo -n " "
-  else if test $__prompt_status -ne 0
-    __bubble $color1 $color0 "$__prompt_status"
-    echo -n " "
-  end
+        command kill \$_tide_last_pid 2>/dev/null
+        set -g _tide_last_pid \$last_pid
+    end
+
+    math \$COLUMNS-(string length -V \"\$$prompt_var[1][1]\$$prompt_var[1][2]\")$column_offset | read -lx dist_btwn_sides
+    string replace @PWD@ (_tide_pwd) $add_newline \$$prompt_var[1][1]'$color_normal '
 end
+
+function fish_right_prompt
+    string unescape \"\$$prompt_var[1][2]$color_normal\"
+end"
+end
+
+eval "function _tide_on_fish_exit --on-event fish_exit
+    set -e $prompt_var
+end"
