5f26fd715bf39b980479edbe1f6c5d2fc1e25ee3
eXenon <xavier.nunn@protonmail.com>
2024-05-12 08:51:48 +0200

    Added ghcup support for manacola

--- fish/conf.host/manacola.fish
+++ fish/conf.host/manacola.fish
@@ -3,3 +3,5 @@ if test -z "$SSH_AGENT_PID"
     eval $(ssh-agent | sed "s/; e/ e/g" | head -n 2)
     ssh-add > /dev/null
 end
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/exenon/.ghcup/bin # ghcup-env
